0f2fece2e3b9294848b125f5ec396a51ff836ff8
İmranov <semtexpert@gmail.com>
2023-12-31 13:16:54 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void islem(int sayi) {
+    System.out.print(sayi + " ");
+
+    if (sayi <= 0)
+    {
+        return;
+    }
+
+    islem(sayi - 5);
+
+    if (sayi > 0)
+    {
+        System.out.print(sayi + " ");
+    }
+}
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        islem(n);
+
     }
 }
